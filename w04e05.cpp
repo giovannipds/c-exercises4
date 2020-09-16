57f28ec359cd94b580fd225e2acf7a8dcef0dad4
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-09-16 17:27:41 -0300

    5

---
+++ w04e05.cpp
@@ -0,0 +1,43 @@
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleOutputCP(1252);
+	
+	int n, i, number;
+	float sqOrSqrt;
+	bool even;
+	
+	// ask for n
+	printf("Digite quantos números você quer ler: ");
+	scanf("%d", &n);
+	n = abs(n);
+	
+	// iterate
+	i = n;
+	while (i > 0)
+	{
+		// ask for number
+		system("cls");
+		printf("Digite um número: ");
+		scanf("%d", &number);
+		
+		// calc
+		even = number % 2 == 0;
+		sqOrSqrt = even ? sqrt(number) : (number*number);
+		
+		// output
+		if (even)
+			printf("Número par, raiz quadrada %.2f.", sqOrSqrt);
+		else
+			printf("Número ímpar, o quadrado de %d é %d.", number, (int)sqOrSqrt);
+		
+		getch();
+		i--;
+	}
+	
+	return 1;
+}
