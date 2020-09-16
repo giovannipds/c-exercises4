ecc4d7cb2fe02d02843a984112291be35af41b9b
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-09-16 16:07:35 -0300

    Add usleep

--- w04e01.cpp
+++ w04e01.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <windows.h>
+#include <unistd.h>
 
 int main()
 {
 	SetConsoleOutputCP(1252);
 	
-	printf("Números inteiros de 100 a 1:\n");
+	printf("Números inteiros de 100 a 1:\n\n");
 	
 	for(int i = 100; i >= 1; i--)
 	{
 		printf("%i", i);
+		usleep(25 * 1000);
 		printf(i != 1 ? ", " : ".");
 	}
 	
